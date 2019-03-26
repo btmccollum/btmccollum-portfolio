8a2e8383c72050fdaea39104b69af4fe279166a0
btmccollum <btmcco8@gmail.com>
2019-03-26 14:25:06 -0500

    adding support for fontawesome, minor updates to home component

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+library.add(fab)
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      // <div className="App">
+      <Container>
         <NavBar />
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/projects" component={Projects} />
         <Route path="/blog" component={Blog} />
         <Route path="/contact" component={Contact} />
-      </div>
+      </Container>
+      // </div>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,33 +2,42 @@ import React from 'react';
 import headshot from '../images/mccollum-headshot.jpg'
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faTwitterSquare faGithubSquare faLinkedin } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fab, faTwitterSquare faGithubSquare faLinkedin)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Contact = () => {
 
     return (
-        <Container>
-            <Row>
-                <Col className="headshot">
-                    <img src={headshot} alt="my headshot" className="me" /> 
-                </Col>
+        <>
+            <Container>
+                <Row>
+                    <Col className="headshot" xs={3}>
+                        <img src={headshot} alt="my headshot" className="me" /> 
+                    </Col>
+    
+                    <Col className="headshotInfo" xs={6}>
+                        <h1>Brad McCollum</h1>
+                        <Row>
+                            <Col className="socials">
+                                <FontAwesomeIcon icon={["fab", "github-square"]} />
+                                <FontAwesomeIcon icon={["fab", "linkedin"]} />    
+                                <FontAwesomeIcon icon={["fab", "twitter-square"]} />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
 
-                <Col className="headshotInfo">
-                    <h1>Brad McCollum</h1>
-                </Col>
-
-                <Col>
-                    <Link to="/about">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/blog">Blog</Link>
-                    <Link to="/contact">Contact Me</Link>
-                </Col>
-            </Row>
-        </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        <Link to="/about">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/blog">Blog</Link>
+                        <Link to="/contact">Contact Me</Link>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 
