3887c1dfc58f7539a31a4df94b54e8b18841667d
btmccollum <brad.mccollum@hey.com>
2022-03-11 20:04:59 -0500

    cleanup component, upgrade dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import ScrollUpButton from 'react-scroll-up-button';
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
-import projectData from './content/projects/project-data';
-
+import NavBar from './components/navbar';
+import Home from './components/home';
+import About from './components/about';
+import Projects from './components/projects';
+import Contact from './components/contact';
+import ScrollUpButton from 'react-scroll-up-button';
+import projectData from './content/projects/projectData';
 import './App.css';
 
 // library for fontawesome icons, importing brand icons
 library.add(fab, faEnvelopeSquare)
 
-
 class App extends Component {
   componentDidMount() {
     console.log('Thanks for taking a look at my portfolio!\nCheck out my other pages too:\n\nWebsite: www.btmccollum.com\n\nGitHub: www.github.com/btmccollum\n\nLinkedIn: https://www.linkedin.com/in/btmccollum/\n\nBlog: https://medium.com/@btmccollum\n\nTwitter: www.twitter.com/btmccollum\n\nEmail: bmccollum.swe@gmail.com\n\n');
   }
-  
+
   render() {
     return (
       <div className="App">
---
+++ src/components/contact.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <Container fluid className="h-100">
+        <Row className="justify-content-center align-content-center h-100">
+          <Col md={3}>
+            {/* section title, content removed at breakpoint */}
+            <div className="contactBlock">
+              <h2 className="margin-b-0 sectionTitle contactTitle">Contact</h2>
+            </div>
+          </Col>
+
+          <Col sm={8}>
+            <Row className="justify-content-center">
+              {/* location info, content removed at breakpoint */}
+              <Col md={3} className="contactBlock location">
+                <h5 className="headerTitle">
+                  Location
+                </h5>
+                Canterbury, CT
+              </Col>
+
+              <Col md={5} sm={6} xs={6} className="contactBlock">
+                <h5 className="headerTitle">Email</h5>
+                <a className="normalEmail" href="mailto:bmccollum.swe@gmail.com">
+                  bmccollum.swe@gmail.com
+                </a>
+                {/* displayed only as smallest media queries */}
+                <a className="miniEmail" href="mailto:bmccollum.swe@gmail.com">
+                  <FontAwesomeIcon icon="envelope-square" size="3x" />
+                </a>
+              </Col>
+
+              <Col md={4} sm={6} xs={6} className="contactBlock socialBlock">
+                <h5 className="headerTitle">Social Media</h5>
+                <a href="https://www.github.com/btmccollum" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={["fab", "github-square"]} size="3x" />
+                </a>
+                <span className="spacer" />
+                <a href="https://www.linkedin.com/in/btmccollum/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" />
+                </a>
+                <span className="spacer" />
+                <a href="https://www.twitter.com/btmccollum" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={["fab", "twitter-square"]} size="3x" />
+                </a>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default Contact;
